9a5ff6e1b72685ddc86f93689dfb3bde0aa1c52c
zhangkun <zhangkun@boxfish.cn>
2017-06-07 14:22:05 +0800

    空安全：安全调用和 Elvis 操作符

--- app/src/main/java/com/mao/cn/kotlinlearn/nullPoint/NullDemo.kt
+++ app/src/main/java/com/mao/cn/kotlinlearn/nullPoint/NullDemo.kt
@@ -1,20 +1,25 @@
 package com.mao.cn.kotlinlearn.nullPoint
 
+import java.io.File
+
 /**
  * Created by zhangkun on 2017/6/5.
  *
  * 可空类型与非空类型
  * 所有数据类型默认都是非空（non-null）的，如果想把 null 赋给它们的对象，需要在类型后加上 ? ，声明为可空类型
  */
-fun main(arg:Array<String>){
+fun main(arg: Array<String>) {
 
-    var mm : String? = null
+    var mm: String? = null
 
     printLengthOfString(mm)
 
     mm = "test"
 
     printLengthOfString(mm)
+
+
+    elvisFun()
 
 
 }
@@ -24,4 +29,16 @@ fun main(arg:Array<String>){
  * 如果一个可空变量已经被判断为非空，再次调用它的方法和变量时，会自动将变量转换为非空类型，实现安全调用
  *
  */
-fun printLengthOfString(s:String?) = println(s?.length)
+fun printLengthOfString(s: String?) = println(s?.length)
+
+
+//------------Elvis 操作符 ?:
+fun elvisFun() {
+
+    val list = ArrayList<String>()
+    println(list?.size ?: "null")
+
+    val listFiles = File("\\nullPoint").listFiles()
+    println(listFiles?.size ?: "Empty")
+
+}
