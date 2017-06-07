974abce7fae3b903f7a374c46c52ab0c161f10b6
zhangkun <zhangkun@boxfish.cn>
2017-06-07 15:19:38 +0800

    空安全：let 函数和 !! 操作符

--- app/src/main/java/com/mao/cn/kotlinlearn/nullPoint/NullDemo.kt
+++ app/src/main/java/com/mao/cn/kotlinlearn/nullPoint/NullDemo.kt
@@ -18,10 +18,11 @@ fun main(arg: Array<String>) {
 
     printLengthOfString(mm)
 
-
     elvisFun()
 
+    letFun()
 
+    notNullFun()
 }
 
 // 安全调用符 ?.
@@ -32,13 +33,44 @@ fun main(arg: Array<String>) {
 fun printLengthOfString(s: String?) = println(s?.length)
 
 
-//------------Elvis 操作符 ?:
+//------------Elvis 操作符 ?:    if null then
 fun elvisFun() {
 
     val list = ArrayList<String>()
     println(list?.size ?: "null")
 
-    val listFiles = File("\\nullPoint").listFiles()
+    val listFiles = File("./folder").listFiles()
     println(listFiles?.size ?: "Empty")
 
+
+    val listFiles1 = File("app/src/main/java/com/mao/cn/kotlinlearn/nullPoint/folder").listFiles()
+    println(listFiles1?.size ?: "Empty")
+
+    val file = File("app/src/main/java/com/mao/cn/kotlinlearn/nullPoint/folder/Test1.kt")
+    println("file   " + file.exists())
+
+}
+
+//---------- let  安全调用符 + let 函数就等同于 if not null then 语句。
+fun letFun() {
+
+    val files = File("app/src/main/java/com/mao/cn/kotlinlearn/nullPoint/folder").listFiles()
+    files?.let {
+        for (file in files) {
+            println(file.name)
+        }
+    }
+}
+
+//----------非空操作符 !!  Kotlin 不建议在代码中广泛使用 !! 操作符，这会浪费 Kotlin 设计良好的非空类型，让 Kotlin 代码充斥着 NPE。
+fun notNullFun() {
+    val files = File("app/src/main/java/com/mao/cn/kotlinlearn/nullPoint/folder").listFiles()!!
+    for (file in files) {
+        println(file.name)
+    }
+    
+    val filesFolder = File("folder").listFiles()!!
+    for (file in filesFolder) {
+        println(file.name)
+    }
 }
