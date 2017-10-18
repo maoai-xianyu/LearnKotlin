ebaeb1b1c7b3cf5191679516e75df23d40edd3ca
maoai_xianyu <194264514@qq.com>
2017-10-18 18:08:19 +0800

    数组，独享，oper

--- app/src/main/java/com/mao/cn/kotlinlearn/K03abstractInterface/InterfaceDemo.kt
+++ app/src/main/java/com/mao/cn/kotlinlearn/K03abstractInterface/InterfaceDemo.kt
@@ -1,13 +1,13 @@
 package com.mao.cn.kotlinlearn.K03abstractInterface
 
-import com.mao.cn.kotlinlearn.K03abstractInterface.impl.KotlinInferfaceImpl
+import com.mao.cn.kotlinlearn.K03abstractInterface.impl.KotlinInterfaceImpl
 
 /**
  * Created by zhangkun on 2017/6/5.
  */
 fun main(args: Array<String>){
 
-    val inter = KotlinInferfaceImpl()
+    val inter = KotlinInterfaceImpl()
     inter.prop = 100
     println(" ----  "+inter.prop)
     inter.foo(1000)
--- app/src/main/java/com/mao/cn/kotlinlearn/K03abstractInterface/impl/KotlinInterfaceImpl.kt
+++ app/src/main/java/com/mao/cn/kotlinlearn/K03abstractInterface/impl/KotlinInterfaceImpl.kt
@@ -5,11 +5,15 @@ import com.mao.cn.kotlinlearn.K03abstractInterface.KotlinInterface
 /**
  * Created by zhangkun on 2017/6/5.
  */
-class KotlinInferfaceImpl : KotlinInterface {
+class KotlinInterfaceImpl : KotlinInterface {
     override var prop: Int = 0
 
     override fun foo(prop: Int) {
         println("prop " + prop)
+    }
+
+    override fun bar() {
+        super.bar()
     }
 
 
