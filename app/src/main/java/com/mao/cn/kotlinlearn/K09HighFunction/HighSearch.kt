44f3fb5fafd2679a5ddad6d45dbc87be657f289b
maoai_xianyu <194264514@qq.com>
2018-05-24 18:23:13 +0800

    Kotlin 学习

--- app/src/main/java/com/mao/cn/kotlinlearn/K09HighFunction/HighSearch.kt
+++ app/src/main/java/com/mao/cn/kotlinlearn/K09HighFunction/HighSearch.kt
@@ -7,6 +7,19 @@ package com.mao.cn.kotlinlearn.K09HighFunction
 
 fun main(array: Array<String>) {
     println(searchFun())
+    println("--------------------------")
+
+
+    var array = intArrayOf()
+    println("null " + searchFirstOrNullFun(array))
+
+    array = intArrayOf(1, 2, 4, 5, 6)
+    println("not null " + searchFirstOrNullFun(array))
+
+    println("--------------------------")
+
+    maxByFun();
+
 }
 
 
@@ -15,4 +28,21 @@ fun searchFun(): Int {
     val array = intArrayOf(1, 5, 3)
 
     return array.findLast { it % 2 == 1 } ?: -1
+}
+
+fun searchFirstOrNullFun(array: IntArray): Int {
+
+    return array.firstOrNull { it % 2 == 1 } ?: -1
+}
+
+
+fun maxByFun() {
+    val stu = arrayOf(
+            Stu("sada", 12),
+            Stu("dfasdfs", 56),
+            Stu("asd", 33)
+    )
+
+    val oldest = stu.maxBy { it.age }!!
+    println("Oldest one is ${oldest.name}")
 }
