f27cac5887b4aff598c30aec0a22c92924423db4
maoai_xianyu <194264514@qq.com>
2018-05-30 16:06:54 +0800

    Kotlin 类的属性的set 和 get

--- app/src/main/java/com/mao/cn/edwardLearn/Kp18Person.kt
+++ app/src/main/java/com/mao/cn/edwardLearn/Kp18Person.kt
@@ -14,6 +14,50 @@ class Kp18Person(val name: String, var age: Int) {
 
 }
 
+
+/**
+ * 只读属性用 val  不允许有setter
+ * 可变属性用 var  有setter  和 getter
+ *
+ * 访问函数创建必须紧跟在属性的声明之后，在属性的访问函数里，不能直接调用属性，只能使用属性的变量名 field代替属性，
+ * 若直接使用属性名，会出现递归，导致堆栈溢出
+ */
+class Kp18Person6(val name: String, var age: Int) {
+
+    var isMarried: Boolean
+        get() = this.name.isEmpty()
+
+
+    var gender: String = "male"
+        get() {
+            println("this is a customized get()")
+            return field
+        }
+        set(value) {
+            println("this is a set")
+            field = value
+        }
+
+    init {
+        println("init Kp18Person5")
+        isMarried = true
+
+    }
+
+    constructor(name: String, age: Int, gender: String) : this(name, age) {
+        println("this is the first secondary constructor.")
+        this.gender = gender
+
+    }
+
+    constructor(name: String, age: Int, gender: String, isMarried: Boolean) : this(name, age, gender) {
+        println("this is the second secondary constructor.")
+        this.isMarried = isMarried
+
+    }
+}
+
+
 // 次构造函数，必须在第一行代码调用主构造函数,中不能出现 参数的声明
 class Kp18Person5(val name: String, var age: Int) {
 
--- app/src/main/java/com/mao/cn/edwardLearn/Kp20PropertiesAndBackFied.kt
+++ app/src/main/java/com/mao/cn/edwardLearn/Kp20PropertiesAndBackFied.kt
@@ -5,5 +5,13 @@ package com.mao.cn.edwardLearn
  * date:    on 2018/5/30.
  */
 fun main(args: Array<String>) {
+    val personA = Kp18Person("jack", 12)
+    println(" name ${personA.name} + age ${personA.age}")
 
+    personA.age = 33
+    println(" name ${personA.name} + age ${personA.age}")
+
+
+    val personB = Kp18Person6("TOM",14,"female")
+    println(" name ${personB.name} + age ${personB.age} + gender ${personB.gender}")
 }
