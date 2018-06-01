540a2fcd1c5f5149f014f4328af22d0dd12b6208
maoai_xianyu <194264514@qq.com>
2018-06-01 19:37:29 +0800

    Kotlin interface

---
+++ app/src/main/java/com/mao/cn/edwardLearn/Kp28SingleTons.kt
@@ -0,0 +1,17 @@
+package com.mao.cn.edwardLearn
+
+/**
+ * author:  zhangkun .
+ * date:    on 2018/6/1.
+ *
+ * object   singleTons  单例
+ *
+ */
+fun main(args: Array<String>) {
+
+}
+
+
+object SingleTonsClass {
+
+}
